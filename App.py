1d6ad0fabc9acffa00784a67d2b86843320fe4e9
DRAG-11 <bundeledurwang@gmail.com>
2023-12-22 11:12:39 +0530

    final commit

---
+++ App.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import re
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/thala", methods=["POST"])
+def thala():
+    str = request.form.get("str")
+    count = len(re.findall("[a-zA-Z]", str))
+    print("Count of Alphabets: ", count)
+    if count == 7:
+        return render_template("index.html", label=1)
+    else:
+        return render_template("index.html", label=-1)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
